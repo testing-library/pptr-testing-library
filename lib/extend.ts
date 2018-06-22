e69a91d2c994186ce26de9d8465a4a7c0b5096d9
Patrick Hulce <patrick.hulce@gmail.com>
2018-06-21 20:05:14 -0700

    feat: add extend export

--- lib/extend.ts
+++ lib/extend.ts
@@ -1,4 +1,4 @@
-import {getTestingUtilsForDocument, createDelegateFor, extendObjectWithTestingUtils} from '.'
+import {getTestingUtilsForDocument, extendObjectWithTestingUtils} from '.'
 
 const Page = require('puppeteer/lib/Page.js')
 const ElementHandle = require('puppeteer/lib/ElementHandle.js')
