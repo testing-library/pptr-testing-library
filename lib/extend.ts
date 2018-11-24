73666d1d05862097aa85e33f7cfa135f2da0d81b
Patrick Hulce <patrick.hulce@gmail.com>
2018-11-24 15:55:59 -0600

    fix: support puppeteer 1.7+

--- lib/extend.ts
+++ lib/extend.ts
@@ -5,9 +5,20 @@ import {IScopedQueryUtils} from './typedefs'
 // tslint:disable-next-line
 let Page, ElementHandle
 
+function requireOrUndefined(path: string): any {
+  try {
+    return require(path)
+  } catch (err) {}
+}
+
 try {
   Page = require('puppeteer/lib/Page.js') // tslint:disable-line
-  ElementHandle = require('puppeteer/lib/ElementHandle.js') // tslint:disable-line
+  if (Page.Page) Page = Page.Page
+
+  ElementHandle = requireOrUndefined('puppeteer/lib/ElementHandle.js') // tslint:disable-line
+  if (!ElementHandle) {
+    ElementHandle = require('puppeteer/lib/ExecutionContext.js').ElementHandle // tslint:disable-line
+  }
 
   Page.prototype.getDocument = getDocument
   getQueriesForElement(ElementHandle.prototype, function(this: ElementHandle): ElementHandle {
