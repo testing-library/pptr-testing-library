984781dcc463bb1b81ea9fa7e6542197712c2c64
Patrick Hulce <patrick.hulce@gmail.com>
2019-05-23 10:16:55 -0500

    tests: add test for `wait`

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,6 @@
 import * as path from 'path'
 import * as puppeteer from 'puppeteer'
-import {getDocument, queries, getQueriesForElement} from '../lib'
+import {getDocument, queries, getQueriesForElement, wait} from '../lib'
 
 describe('lib/index.ts', () => {
   let browser: puppeteer.Browser
@@ -23,6 +23,16 @@ describe('lib/index.ts', () => {
     const element = await getByText('Hello h1')
     expect(await queries.getNodeText(element)).toEqual('Hello h1')
   })
+
+  it('should load the defered page', async () => {
+    await page.goto(`file://${path.join(__dirname, 'fixtures/late-page.html')}`)
+  })
+
+  it('should use `wait` properly', async () => {
+    const {getByText} = getQueriesForElement(await getDocument(page))
+    await wait(() => getByText('Loaded!'))
+    expect(await getByText('Loaded!')).toBeTruthy()
+  }, 7000)
 
   afterAll(async () => {
     await browser.close()
