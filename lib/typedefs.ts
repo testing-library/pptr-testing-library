8afb2e7813553dc17b6c40ff011811da5258cc4e
Tom Ross <tom@umpox.com>
2020-08-18 10:45:38 +0100

    Lint

--- lib/typedefs.ts
+++ lib/typedefs.ts
@@ -8,57 +8,121 @@ interface IQueryMethods {
   queryAllByPlaceholderText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
   getByPlaceholderText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element>
   getAllByPlaceholderText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
-  findByPlaceholderText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByPlaceholderText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByPlaceholderText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByPlaceholderText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element | null>
   queryAllByText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element[]>
   getByText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element>
   getAllByText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element[]>
-  findByText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByLabelText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element | null>
   queryAllByLabelText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element[]>
   getByLabelText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element>
   getAllByLabelText(el: Element, m: Matcher, opts?: SelectorMatcherOptions): Promise<Element[]>
-  findByLabelText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByLabelText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByLabelText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByLabelText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByAltText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element | null>
   queryAllByAltText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
   getByAltText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element>
   getAllByAltText(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
-  findByAltText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByAltText(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByAltText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByAltText(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByTestId(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element | null>
   queryAllByTestId(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
   getByTestId(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element>
   getAllByTestId(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
-  findByTestId(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByTestId(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByTestId(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByTestId(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByTitle(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element | null>
   queryAllByTitle(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
   getByTitle(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element>
   getAllByTitle(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
-  findByTitle(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByTitle(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByTitle(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByTitle(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByRole(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element | null>
   queryAllByRole(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
   getByRole(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element>
   getAllByRole(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
-  findByRole(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByRole(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByRole(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByRole(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 
   queryByDisplayValue(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element | null>
   queryAllByDisplayValue(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
   getByDisplayValue(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element>
   getAllByDisplayValue(el: Element, m: Matcher, opts?: MatcherOptions): Promise<Element[]>
-  findByDisplayValue(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element>
-  findAllByDisplayValue(el: Element, m: Matcher, opts?: SelectorMatcherOptions, waitForOpts?: WaitForElementOptions): Promise<Element[]>
+  findByDisplayValue(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element>
+  findAllByDisplayValue(
+    el: Element,
+    m: Matcher,
+    opts?: SelectorMatcherOptions,
+    waitForOpts?: WaitForElementOptions): Promise<Element[]>
 }
 
 export type BoundFunction<T> = T extends (
@@ -72,8 +136,8 @@ export type BoundFunction<T> = T extends (
   ? (text: P, options?: Q, waitForElementOptions?: W) => R
   : T extends (a1: any, text: infer P, options: infer Q) => infer R
   ? (text: P, options?: Q) => R
-  : never;
-export type BoundFunctions<T> = { [P in keyof T]: BoundFunction<T[P]> };
+  : never
+export type BoundFunctions<T> = { [P in keyof T]: BoundFunction<T[P]> }
 
 export interface IScopedQueryUtils extends BoundFunctions<IQueryMethods> {
   getQueriesForElement(): IScopedQueryUtils
