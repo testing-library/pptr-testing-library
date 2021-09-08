9a205eaee1826b6707c137dacd19d9a908c68d16
georgehb <32330689+georgehb@users.noreply.github.com>
2021-09-08 19:46:06 +0100

    Update lib/index.ts
    Co-authored-by: Patrick Hulce <patrick.hulce@gmail.com>

--- lib/index.ts
+++ lib/index.ts
@@ -31,7 +31,7 @@ function convertProxyToRegExp(o: any, depth: number): any {
 
 function convertRegExpToProxy(o: any, depth: number): any {
   if (typeof o !== 'object' || !o || depth > 2) return o
-  if (Object.prototype.toString.call(o) !== "[object RegExp]") {
+  if (Object.prototype.toString.call(o) !== '[object RegExp]') {
     const copy = {...o}
     for (const key of Object.keys(copy)) {
       copy[key] = convertRegExpToProxy(copy[key], depth + 1)
