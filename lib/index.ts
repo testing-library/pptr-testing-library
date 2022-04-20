cd90422e8186501c03f04d13e9b7d724242e0468
Andre Wiggins <andwi@microsoft.com>
2022-04-20 01:57:01 -0700

    Revert execution context change since it was slower

--- lib/index.ts
+++ lib/index.ts
@@ -1,6 +1,6 @@
 import {readFileSync} from 'fs'
 import * as path from 'path'
-import {ElementHandle, EvaluateFn, ExecutionContext, JSHandle, Page} from 'puppeteer'
+import {ElementHandle, EvaluateFn, JSHandle, Page} from 'puppeteer'
 import waitForExpect from 'wait-for-expect'
 
 import {IConfigureOptions, IQueryUtils, IScopedQueryUtils} from './typedefs'
@@ -117,8 +117,6 @@ interface IHandleSet {
   argsToForward: any[]
 }
 
-const executionContextInit = new WeakMap<ExecutionContext, boolean>();
-
 function createDelegateFor<T = DOMReturnType>(
   fnName: keyof IQueryUtils,
   contextFn?: ContextFn,
@@ -131,14 +129,9 @@ function createDelegateFor<T = DOMReturnType>(
     // @ts-ignore
     const containerHandle: ElementHandle = contextFn ? contextFn.apply(this, args) : this
 
-    // const shouldIncludeTestingLib = await containerHandle.evaluate(
-    //   () => (window as any).__dom_testing_library__ == null
-    // );
-    const executionContext = containerHandle.executionContext();
-    const shouldIncludeTestingLib = !executionContextInit.has(executionContext);
-    if (shouldIncludeTestingLib) {
-      executionContextInit.set(executionContext, true);
-    }
+    const shouldIncludeTestingLib = await containerHandle.evaluate(
+      () => (window as any).__dom_testing_library__ == null
+    );
 
     // @ts-ignore
     const evaluateFn: EvaluateFn = new Function(
